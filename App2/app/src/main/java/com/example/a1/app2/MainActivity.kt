78553219a75334b1103209e546b06fdfd095b5bc
F1L1N <pavel9696@yandex.ru>
2018-06-16 13:46:51 +0300

    Added crating intent in companion obj

--- App2/app/src/main/java/com/example/a1/app2/MainActivity.kt
+++ App2/app/src/main/java/com/example/a1/app2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a1.app2
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,10 +8,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+
+        fun createStartIntentAct1(context: Context): Intent {
+            return Intent(context, Activity1::class.java)
+        }
+
+        fun createStartIntentAct2(context: Context): Intent {
+            return Intent(context, Activity2::class.java)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1.setOnClickListener { startActivity(Intent(this, Activity1::class.java)) }
-        button2.setOnClickListener { startActivity(Intent(this, Activity2::class.java)) }
+        button1.setOnClickListener { startActivity(createStartIntentAct1(this)) }
+        button2.setOnClickListener { startActivity(createStartIntentAct2(this)) }
     }
 }
